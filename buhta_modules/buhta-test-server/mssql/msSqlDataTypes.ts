a0fa7601e51b30a4e86b1374e446406577a52b27
KostiaSA <savchenkov@buhta.ru>
2017-01-05 15:16:13 +0300

    --

--- buhta_modules/buhta-test-server/mssql/msSqlDataTypes.ts
+++ buhta_modules/buhta-test-server/mssql/msSqlDataTypes.ts
@@ -9,11 +9,14 @@ export async function msSqlDataTypes(): Promise<void> {
 
             let sql = `
 SELECT
+   CONVERT(IMAGE,0x652378AAAAAAAAAAAAAAAAA3636723) IMAGE_COLUMN,
+   newid() GUID_COLUMN,             
    CONVERT(VARCHAR,'qwerty') VARCHAR_COLUMN,             
    CONVERT(NVARCHAR,'qwerty1') NVARCHAR_COLUMN,
    CONVERT(CHAR,'костя') CHAR_COLUMN,             
    CONVERT(NCHAR,'мостя') NCHAR_COLUMN,
    CONVERT(TEXT,'I currently have a resultset returned and in one of the columns the string value may be null') TEXT_COLUMN,
+   CONVERT(NTEXT,'NNN I currently have a resultset returned and in one of the columns the string value may be null') NTEXT_COLUMN,
                 
    CONVERT(BIT,1) BIT_COLUMN,
    CONVERT(BIT,0) BIT_COLUMN_FLASE,
@@ -27,13 +30,13 @@ SELECT
    CONVERT(MONEY,9999.99125) MONEY_COLUMN,
 --   null NULL_COLUMN,
    
-   CONVERT(IMAGE,0x652378AAAAAAAAAAAAAAAAA3636723) IMAGE_COLUMN,
    
    CONVERT(DATE,'2016-01-01') DATE_COLUMN,             
    CONVERT(TIME,'16:12:11.234456') TIME_COLUMN,             
    CONVERT(DATETIME,'2016-01-01 18:19:20.15') DATETIME_COLUMN,             
    
    SYSDATETIMEOFFSET() SYSDATETIMEOFFSET,  
+   --CONVERT(SQL_VARIANT,9999.99125) VARIANT_COLUMN, 
    
    'fake' fake
           
